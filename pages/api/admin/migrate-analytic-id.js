0701c3fe736d1741889cb6baf7edbed8695a8ea6
Auto Deployer <auto@chryso.app>
2026-04-10 15:12:35 +0200

    Fix import path in migrate-analytic-id.js

--- pages/api/admin/migrate-analytic-id.js
+++ pages/api/admin/migrate-analytic-id.js
@@ -1,4 +1,4 @@
-import { getPool } from '../../services/db'
+import { getPool } from '../../../services/db'
 
 export default async function handler(req, res) {
   // Security: Only allow in development or with a migration token
