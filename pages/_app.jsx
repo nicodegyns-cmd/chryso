fa10206319a34f6ea90421ec74f8d0ca6f116d6f
Auto Deployer <auto@chryso.app>
2026-04-14 21:33:26 +0200

    revert: suppression du footer bas de page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,39 +1,7 @@
 import '../styles/globals.css'
 import '../pages/admin/rib-validation.module.css'
 import '../pages/documents.module.css'
-import Image from 'next/image'
-
-function DevFooter() {
-  return (
-    <footer style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      gap: 0,
-      padding: '0 20px',
-      background: '#1a1a2e',
-      borderTop: '1px solid #2d2d4e',
-      marginTop: 'auto',
-      height: 56,
-    }}>
-      <span style={{ color: '#9ca3af', fontSize: 12, whiteSpace: 'nowrap' }}>Développé par</span>
-      <Image
-        src="/nexio-seven-logo.png"
-        alt="Nexio Seven"
-        width={220}
-        height={56}
-        style={{ objectFit: 'contain', display: 'block', marginLeft: -16 }}
-        priority={false}
-      />
-    </footer>
-  )
-}
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Component {...pageProps} />
-      <DevFooter />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
